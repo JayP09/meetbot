bf660ac2acf01f9e2c9ab40b24ce480c667d3e08
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-05-05 20:09:00 +0530

    third push

--- Google meet/content.js
+++ Google meet/content.js
@@ -1,12 +1,12 @@
-chrome.runtime.onMessage.addListener(addData)
+chrome.runtime.onMessage.addListener(
 
-function addData(linkData) {
+function (request, sender, sendResponse) {
     console.log(linkData);
     let maindiv = document.querySelector(".incoming");
     console.log(maindiv)
     let links = document.createElement("div");
     links.classList.add(".bubble");
-    let link = document.createTextNode(linkData.link);
+    let link = document.createTextNode(request.link);
     links.appendChild(link);
     maindiv.appendChild(links);
-}
+});
